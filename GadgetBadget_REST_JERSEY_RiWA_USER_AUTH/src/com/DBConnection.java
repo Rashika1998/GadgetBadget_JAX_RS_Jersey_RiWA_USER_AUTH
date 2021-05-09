d80fb2724422dd479a798c9e71473e4f1f09a107
Rashika1998 <rashikamr1998@gmail.com>
2021-05-09 21:49:21 +0530

    Database connection has been changed(password).

--- GadgetBadget_REST_JERSEY_RiWA_USER_AUTH/src/com/DBConnection.java
+++ GadgetBadget_REST_JERSEY_RiWA_USER_AUTH/src/com/DBConnection.java
@@ -14,7 +14,7 @@ public class  DBConnection {
 			Class.forName("com.mysql.jdbc.Driver"); 
 			// Change your configurations settings
 			//ex: Password, Username
-			dBConnection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget_rest_jersey", "root", "issa123"); 
+			dBConnection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/gadgetbadget_rest_jersey", "root", ""); 
 		} catch (Exception e)  {e.printStackTrace();} 
 		return dBConnection; 
 	}
